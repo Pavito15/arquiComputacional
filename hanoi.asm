942bc2df88ea89af51f2a50c36ed8ee190d5a221
PerlaGar <74082822+PerlaGar@users.noreply.github.com>
2023-10-25 22:49:31 -0600

    Practica 1 Hanoi terminada

--- hanoi.asm
+++ hanoi.asm
@@ -1,3 +1,6 @@
+#Perla Marina García García is728149
+#Paulina Valadez Fernández  is728412
+
 #void hanoi(int n, char origen, char destino, char auxiliar) {
 #    if (n == 1) {
 #        printf("Mover disco 1 desde %c a %c\n", origen, destino);
@@ -10,49 +13,45 @@
 #    hanoi(n - 1, auxiliar, destino, origen);
 #}	
 #32 bits cada palabra = 4 bytes 
-
-# 1.- Mover parte superior a Auxiliar 
-# 2.- Mover pieza actual a Destino
-# 3.- Mover parte superior al Destino
 #Mover los N-1 discos superiores de la torre de A a B, siendo B la posición que no corresponde ni al origen ni al destino.
 #Mover el disco restante (el más grande) de A a C.
 #Mover los N-1 discos de B a C, colocándolos encima del disco de mayor diámetro.
 .text 
-main:   addi s0, zero, 3  # numero de discos 
-	add s2, s0, zero #copia de mi n 
+main:   addi s0, zero, 3 # numero de discos 
+	add s2, s0, zero  # copia de n 
 	addi t2, zero,  4 # espacio de cada disco 
-	addi t0, zero, 1 # es mi limite del ciclo 
-	lui s1, 0x10010 #apunta a la dirección más baja de la 1001 
+	addi t0, zero, 1  # es mi limite del ciclo 
+	lui s1, 0x10010   # apunta a la dirección más baja de la 1001 
 	
-ciclo:  sw s0, 0(s1)  #escribe el disco en la dirección 
-	addi s0, s0, -1 #reducir n-1 el numero de discos 
-	addi s1, s1, 4 # sumo 4 para pasar a la sig dirección
+ciclo:  sw s0, 0(s1)      # escribe el disco en la dirección 
+	addi s0, s0, -1   # reducir n-1 el numero de discos 	
+	addi s1, s1, 4    # sumo 4 para pasar a la sig dirección
 	bge s0, t0, ciclo # si no son iguales repites el ciclo if(s0 != 1)
-	slli t1, s2,2 #n*2
-	slli t1, s2,2 #n*2
+	slli t1, s2,2     # n*2
+	slli t1, s2,2     # n*2
 
 #s1 -> queda con la dirección de aux 
 #a2 -> n = Numero de discos
-	add a2, zero, s2 #n = Numero de discos  
+	add a2, zero, s2  # n = Numero de discos  
 #a3 -> origen = A
-	sub a3,s1,t1 #origen, resto s1 -(n*4) para apuntar a la base origen 
+	sub a3,s1,t1      # origen, resto s1 -(n*4) para apuntar a la base origen 
 #a4 -> aux = B
 	add a4, zero, s1 # aux s1 apunta a la siguiente direccion se multiplica por zero por que ahi empieza el aux
 #a5 -> destino = C 
-	add a5, s1, t1 #destino, sumo s1+(n*4) para apuntar a la base destino 
+	add a5, s1, t1   # destino, sumo s1+(n*4) para apuntar a la base destino 
 #          s1-4,a3, a5 
 	jal hanoi
-	jal end
+	jal end 
 
-###############################################
+################################################################
 # 1. Mover parte superior a NO destino
     #1.1 Ajustar apuntadores (Reducir Origen y Aumentar Destino)
 # 2. Mover pieza actual a destino
 # 3. Mover parte superior a destino
 
-hanoi:  beq a2, t0, if # if (n == 1)
+hanoi:    beq a2, t0, if   # if (n == 1)
 #    hanoi(n - 1, origen, auxiliar, destino); 
-#antes de llamar a la función guardo mis datos para no perderlos
+#antes de llamar a la función guardo mis datos en el stack pointer para no perderlos
 llamada1: addi sp, sp, -20 #push 
 	sw a5, 16(sp)#destino
 	sw a4, 12(sp)#aux
@@ -84,15 +83,14 @@ llamada1: addi sp, sp, -20 #push
 	sw ra, 0 (sp)#dirección de regreso
 	#modificar argumento
 	addi a2, zero, 1#n=1
-llamada2:jal hanoi
+llamada2:jal hanoi  #ya modificados hago la 2da llamada recursiva
 	#pop 
-	lw ra, 0(sp)#return address
+	lw ra, 0(sp)#dirección de regreso
 	lw a2, 4(sp)#n
 	lw a3, 8(sp)#origen
 	lw a4, 12(sp)#aux
 	lw a5, 16(sp)#destino
 	addi sp, sp, +20
-#	jalr ra
 
 #3ra llamada
 #    hanoi(n - 1, auxiliar, destino, origen);
@@ -103,16 +101,16 @@ llamada2:jal hanoi
 	sw a4, 12(sp)#aux
 	sw a3, 8(sp) #origen
 	sw a2, 4(sp) # n
-	sw ra, 0 (sp) 
+	sw ra, 0 (sp) #dirección de regreso
 	#modificar parametros
 llamada3:sub a2, a2, t0 # n-1 
 	lw a4, 8(sp)#origen = aux
 	lw a3, 12(sp)#aux = origen
 	#destino tiene el mismo valor y ya fue obtenido en la llamada anterior
 	addi sp, sp, +20
-	jal hanoi 
+	jal hanoi  #ya modificados hago la 3ra llamada recursiva
 	#pop 
-	lw ra, 0(sp)#return address
+	lw ra, 0(sp)#dirección de regreso
 	lw a2, 4(sp)#n
 	lw a3, 8(sp)#origen
 	lw a4, 12(sp)#aux
